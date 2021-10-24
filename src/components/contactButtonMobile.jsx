463d9b1d404ecc7a87b06d2a47089a058956665d
Batyrkhan Yerekeyev <batyrkhan.yerekeyev@nu.edu.kz>
2021-10-24 08:56:56 +0600

    chenged styling of contactButtonMobile

--- src/components/contactButtonMobile.jsx
+++ src/components/contactButtonMobile.jsx
@@ -1,31 +1,36 @@
 import {Mobile} from "./MediaContainer";
 import style from '../style/contactButtonMobile.module.css'
-import {useRef, useState} from "react";
 import {ReactComponent as WhatsApp} from "../static/icons/whatsapp.svg";
 import {ReactComponent as Phone} from "../static/icons/phone.svg";
-import {ReactComponent as ChevronDown} from "../static/icons/expand_more.svg";
 import {phoneLink, whatsAppLink} from "../utils/constants";
 import classNames from "classnames";
-import useOutsideAlert from "../utils/useOutsideAlert";
+// import {useRef, useState} from "react";
+// import useOutsideAlert from "../utils/useOutsideAlert";
+// import {ReactComponent as ChevronDown} from "../static/icons/expand_more.svg";
 
 const ContactButtonMobile = () => {
-	const [isOpen, setIsOpen] = useState(false)
-	const ref = useRef()
-	const unFocus = () => setIsOpen(false)
-	const toggleFocus = () => setIsOpen(prev => !prev)
-	useOutsideAlert(ref, unFocus)
+	// const [isOpen, setIsOpen] = useState(false)
+	// const ref = useRef()
+	// const unFocus = () => setIsOpen(false)
+	// const toggleFocus = () => setIsOpen(prev => !prev)
+	// useOutsideAlert(ref, unFocus)
 	return (
 		<Mobile>
-			<div ref={ref} className={style.container}>
-				<button onClick={toggleFocus} className={classNames(style.circle, style.filler, {[style.open]: isOpen})}>
-					{isOpen && (
-						<ChevronDown/>
-					)}
-				</button>
+			<div
+				// ref={ref}
+				className={style.container}
+			>
+				{/*<button onClick={toggleFocus} className={classNames(style.circle, style.filler, {[style.open]: isOpen})}>*/}
+				{/*	{isOpen && (*/}
+				{/*		<ChevronDown/>*/}
+				{/*	)}*/}
+				{/*</button>*/}
 				<a
 					href={phoneLink}
 					rel={'noreferrer'}
-					className={classNames(style.circle, style.phone, {[style.open]: isOpen})}
+					className={classNames(style.circle, style.phone,
+						// {[style.open]: isOpen}
+					)}
 				>
 					<Phone/>
 				</a>
@@ -33,7 +38,9 @@ const ContactButtonMobile = () => {
 					href={whatsAppLink}
 					target={'_blank'}
 					rel={'noreferrer'}
-					className={classNames(style.circle, style.whatsApp, {[style.open]: isOpen})}
+					className={classNames(style.circle, style.whatsApp,
+						// {[style.open]: isOpen}
+					)}
 				>
 					<WhatsApp/>
 				</a>
